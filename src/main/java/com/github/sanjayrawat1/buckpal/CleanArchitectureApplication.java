5d1cea76118de9d72330b45e6036c891bc3eb240
Sanjay Singh Rawat <s.rawat1339@gmail.com>
2024-06-20 15:50:00 +0530

    add spring boot maven project

---
+++ src/main/java/com/github/sanjayrawat1/buckpal/CleanArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.github.sanjayrawat1.buckpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanArchitectureApplication.class, args);
+	}
+
+}
